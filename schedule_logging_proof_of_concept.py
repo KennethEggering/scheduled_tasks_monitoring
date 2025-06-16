6cd5250ab7df2282f257f476205df569bc4bd8a5
KennethEggering <kenneth.eggering@stantec.com>
2025-06-16 14:36:25 -0700

    Update schedule_logging_proof_of_concept.py
    had invalid html content

--- schedule_logging_proof_of_concept.py
+++ schedule_logging_proof_of_concept.py
@@ -77,7 +77,6 @@ print(f"Feature-Layer '{scheduled_tasks_flyr_name}' has a spatial-reference of {
 cur_dir = os.curdir
 
 try:
-    # file_ = os.path.abspath(__file__)
     file_ = os.path.abspath(inspect.getfile(inspect.currentframe()))
 except Exception as f_ex:
     file_ = f"Unknown file due to error: {f_ex}"
